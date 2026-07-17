15764bd86e4337bc95a98dcc46744f817e7fbce9
Peter Mekis <mekis.peter@gmail.com>
2026-07-17 23:42:30 +0300

    flutter upgrade

--- frontend/lib/config/api_config.dart
+++ frontend/lib/config/api_config.dart
@@ -1,6 +1,8 @@
 class ApiConfig {
+  // normal dev: local hosting
   static const String debugBaseUrl = 'http://127.0.0.1:8000';
-
+  // demo: hosting from server
+  // static const String debugBaseUrl = 'https://tracking.mekis.dev';
   static const String baseUrl = String.fromEnvironment(
     'API_BASE_URL',
     defaultValue: debugBaseUrl,
